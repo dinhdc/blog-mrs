6728d9cd783cb3ee3049a330d311ca72304a4790
congdinh <congdinh.work@gmail.com>
2023-12-20 20:24:55 +0700

    init

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import PostCreate from "./PostCreate.jsx";
+import PostList from "./PostList.jsx";
+
+function App() {
+
+    return (
+        <div className="container">
+            <h1>Create Post</h1>
+            <PostCreate/>
+
+            <hr/>
+            <h1>Posts</h1>
+            <PostList />
+        </div>
+    )
+}
+
+export default App
---
+++ posts/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const cors = require('cors')
+const {randomBytes} = require('crypto');
+
+const app = express();
+app.use(express.json())
+app.use(cors());
+
+let posts = {}
+
+app.get('/posts', (req, res) => {
+    res.send(posts)
+});
+
+app.post('/posts', (req, res) => {
+    const id = randomBytes(4).toString('hex');
+    const {title} = req.body;
+
+    posts[id] = {
+        id, title
+    }
+
+    res.status(201).send(posts[id]);
+});
+
+app.listen(4000, () => {
+    console.log('Post services run on port 4000')
+})
